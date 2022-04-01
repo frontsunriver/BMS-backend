a4d7dfdb81e778736848f14bca28760f389c2afd
sunriver <neymarjohn215@gmail.com>
2022-04-01 03:01:11 +0300

    feat: add contact us page

---
+++ application/views/contactus.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Support for Smart OA</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="robots" content="noindex" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+<link rel="stylesheet" href="<?php echo ASSET_URL;?>styles/live.css"/>
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="<?php echo ASSET_URL;?>images/favicon.ico" />
+</head>
+<body>
+<div class="page">
+<div class="header">
+<div class="container">
+<p class="title">Smartoa</p>
+</div>
+</div>
+
+
+<div class="translations-content-container">
+<div class="container">
+<div class="tab-content translations-content-item en visible" id="en">
+<br>
+<p>Thank you for choosing our app for managing your community. Please find below contact details</p>
+<p>For any support related queries please email:<a href="mailto:admin@smartoaadmin.com" target="_blank">admin@smartoaadmin.com</a></p>
+</div>
+</div>
+</div>
+
+</div>
+
+<script type="text/javascript">
+    "use strict";
+    window.LCG_TRACKING_APPLICATION = "termsfeed-livelink";
+    window.LCG_TRACKING_ENVIRONMENT = "production";
+    window.LCG_TRACKING_EPOCH = "2020-e01";
+</script>
+
+</body>
+</html>
